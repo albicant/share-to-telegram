5474f0fbccc39770e308dc6c5e084eccd62b9882
G. Sytov <silvics@protonmail.com>
2018-08-12 00:16:03 -0700

    Delete background.js

--- src/background.js
+++
@@ -1,9 +0,0 @@
-function shareToTelegram(data) {
-    browser.tabs.create({
-        url: 'https://telegram.me/share/url?url=' + data
-    });
-}
-
-browser.browserAction.onClicked.addListener((currentTab) => {
-    shareToTelegram(currentTab.url)
-});
