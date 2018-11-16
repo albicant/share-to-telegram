9751c0d30074c86a570eac561bf5bb43400456d7
G. Sytov <silvics@protonmail.com>
2018-11-16 15:37:01 -0800

    The telegram-share tab is now closing automatically
    Added the functionality that the telegram-share tab is now closing automatically. No need to close it manually.

--- background.js
+++ background.js
@@ -1,9 +1,22 @@
+var shareToTelegramTabId = '';
+
 function shareToTelegram(data) {
-    browser.tabs.create({
+    let shareToTelegramTab = browser.tabs.create({
         url: 'https://telegram.me/share/url?url=' + data
     });
+    shareToTelegramTab.then((tab) => {
+        shareToTelegramTabId = tab.id;
+    });
+}
+
+function handleUpdated(tabId, changeInfo, tabInfo) {
+    if(shareToTelegramTabId == tabId && tabInfo.status == "complete") {
+        browser.tabs.remove(shareToTelegramTabId);
+        shareToTelegramTabId = '';
+    }
 }
 
 browser.browserAction.onClicked.addListener((currentTab) => {
     shareToTelegram(currentTab.url)
 });
+browser.tabs.onUpdated.addListener(handleUpdated);
